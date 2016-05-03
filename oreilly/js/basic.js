0057d81fc510dbe6778ba9260e0895af38ca0374
Victor Novy <novyvictor@gmail.com>
2016-05-02 22:21:57 -0300

    -Basic introduction to ng-repeat, ng-class, ng-show

--- oreilly/js/basic.js
+++ oreilly/js/basic.js
@@ -1,11 +1,46 @@
 var basicApp = angular.module('basicApp', []);
 
 basicApp.controller('MainCtrl', [function() {
-    self = this;
+    var self = this;
     self.hello = 'Hello ' + (self.hello || 'User');
 
     self.message = 'This is a message.';
     self.changeMessage = function() {
         self.message = 'Ops! Changed message!!';
+    };
+}]);
+
+basicApp.controller('RepeatCtrl', [function() {
+    var self = this;
+    self.notes = [
+        {
+            "id": 1,
+            "label": "First Note",
+            "done": false
+        },
+        {
+            "id": 2,
+            "label": "Second Note",
+            "done": false,
+            "assignee": "Shyam"
+        },
+        {
+            "id": 3,
+            "label": "Done Note",
+            "done": true
+        },
+        {
+            "id": 4,
+            "label": "Last Note",
+            "done": false,
+            "assignee": "Brad"
+        }
+    ];
+
+    self.getNoteClass = function(status) {
+        return {
+            "done": status,
+            "pending": !status
+        };
     };
 }]);
